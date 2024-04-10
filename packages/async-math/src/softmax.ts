4cc4ba720267ba7a956774d075c194ff77690e83
miaobuao <miaobuao@outlook.com>
2024-04-10 18:47:27 +0800

    feat: softmax support MxN Matrix

--- packages/async-math/src/softmax.ts
+++ packages/async-math/src/softmax.ts
@@ -1,15 +1,20 @@
-import type { MathCollection } from 'mathjs'
+import type { MathCollection, Matrix } from 'mathjs'
 
-import { divide, exp, map, max, subtract, sum } from 'mathjs'
+import { divide, exp, isArray, map, matrix, max, subtract, sum } from 'mathjs'
 
-/**
- *
- * @param a vector like [0, 1, 2, ...]
- * @returns vector like [0, 1, 2, ...]
- */
 export async function softmax(a: MathCollection) {
-  const c = max(a)
-  a = map(a, d => exp(subtract(d, c)))
-  const sumed = sum(a)
-  return a.map(d => divide(d, sumed))
+  if (isArray(a))
+    a = matrix(a)
+  if (a.size().length > 1) {
+    const c = max(a, 1) as unknown as Matrix
+    a = a.map((d, [ idx ]) => exp(subtract(d, c.get([ idx! ]))))
+    const sumed = sum(a, 1) as unknown as Matrix
+    return a.map((d, [ idx ]) => divide(d, sumed.get([ idx! ])))
+  }
+  else {
+    const c = max(a)
+    a = map(a, d => exp(subtract(d, c)))
+    const sumed = sum(a)
+    return a.map(d => divide(d, sumed))
+  }
 }
