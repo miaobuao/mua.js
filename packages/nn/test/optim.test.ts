2c48c23ca181c20ddb6d124c8493ab228500c59e
miaobuao <miaobuao@outlook.com>
2024-04-15 06:14:06 +0800

    use ReLU

--- packages/nn/test/optim.test.ts
+++ packages/nn/test/optim.test.ts
@@ -2,7 +2,7 @@
 
 import type { Tensor } from '@mua/tensor'
 
-import { flatten, random } from '@mua/tensor'
+import { flatten, random, relu } from '@mua/tensor'
 import { range } from 'lodash-es'
 import { describe, it } from 'vitest'
 
@@ -12,17 +12,18 @@ describe('optim', () => {
   const l2loss = new L2Loss()
   it.concurrent('optim linear', async () => {
     class MyModel extends Module {
-      readonly linear1 = new Linear(20, 10)
+      readonly linear1 = new Linear(100, 10)
       readonly linear2 = new Linear(10, 4)
       forward(input: Tensor) {
         const x1 = this.linear1.forward(input)
-        return this.linear2.forward(x1)
+        const x2 = relu(x1)
+        return this.linear2.forward(x2)
       }
     }
     const model = new MyModel()
     const sgd = new SGD(model.parameters(), 1e-3)
-    const x = random([ 10, 20 ], 0, 1)
-    const y = random([ 10, 4 ], 0, 1)
+    const x = random([ 20, 100 ])
+    const y = random([ 20, 4 ])
     const losses: any[] = []
     for (const e of range(5)) {
       sgd.resetGrad()
@@ -37,17 +38,18 @@ describe('optim', () => {
 
   it.concurrent('optim conv1d', async () => {
     class MyModel extends Module {
-      readonly conv1 = new Conv1d(3, 16, 3, { padding: 1 })
-      readonly conv2 = new Conv1d(16, 8, 3, { padding: 1 })
+      readonly conv1 = new Conv1d(10, 16, 3, { padding: 1 })
+      readonly conv2 = new Conv1d(16, 4, 3, { padding: 1 })
       forward(input: Tensor) {
         const x1 = this.conv1.forward(input)
-        return this.conv2.forward(x1)
+        const x2 = relu(x1)
+        return this.conv2.forward(x2)
       }
     }
     const model = new MyModel()
     const sgd = new SGD(model.parameters(), 1e-3)
-    const x = random([ 5, 3 ])
-    const y = random([ 5, 8 ])
+    const x = random([ 20, 10 ])
+    const y = random([ 20, 4 ])
     const losses: any[] = []
     for (const e of range(5)) {
       sgd.resetGrad()
